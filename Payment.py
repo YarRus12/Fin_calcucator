6c8e68ad8dc192703f9cc650090f566f53fd3b84
Yaroslav Russu <97214900+YarRus12@users.noreply.github.com>
2022-04-28 21:31:43 +1000

    Подготовлен расчет ануитетного платежа Подготовлен расчет изменения остатка платежа  - Смонтировать изменение месяца  - Смотрировать расчет по дням  - Смонтировать возможность вненсения изменений в определенный месяц

--- Payment.py
+++ Payment.py
@@ -1,6 +1,6 @@
-import datetime
+from datetime import datetime, timedelta
 #from datetime import relativedelta
-import dateutil
+#import dateutil
 
 def annuity_payment(ammount, procents, month):
     """Формула расчета аннуитетного платежа"""
@@ -8,26 +8,38 @@ def annuity_payment(ammount, procents, month):
     pay_ammount = ammount*(procent/(1-(1+procent)**(-(month-1))))
     return pay_ammount
 
-def full_payment(ammount, procents, months, day_mon_year):
+def calendar(date_in):
+    year, month, day = date_in
+    return datetime(year, month, day)
+
+
+def next_date(date):
+    date = str(date).split(' ')
+    year, month, day = map(int, str(date[0]).split('-'))
+    if month < 12:
+        month += 1
+    else:
+        month = 1
+        year += 1
+    return datetime(year, month, day)
+
+
+def full_payment(ammount, procents, months, end_of_period, day_mon_year):
     summ = ammount
-    mounth_num = 0
+    date = calendar(day_mon_year)
+    end_date = calendar(end_of_period)
     while summ > 0:
+        date = next_date(date)
         payment = annuity_payment(ammount, procents, months)
         #Здесь следует переправить переменную year для избежания ValueError: month must be in 1..12
-        day, month, year = day_mon_year
-        if month < 12:
-            month += mounth_num
-        else:
-            month = 1
-            year += 1
-        date = datetime.date(day, month, year).isoformat()
         print(f'{date}, размер платежа составит {round(payment, 2)}, остаток по кредиту составляет {round(summ, 2)}')
         summ -= payment
-        mounth_num += 1
+
+
 
 #print('Today: ', datetime.today().strftime('%d/%m/%Y'))
 #print('After Month:', date_after_month.strftime('%d/%m/%Y'))
 
 #def payment():
-
-full_payment(748609, 7.91, 82, (2022, 2, 15))
+#print(calendar((2022, 2, 15)))
+full_payment(748609, 7.91, 81, (2027, 2, 15), (2022, 2, 15))
